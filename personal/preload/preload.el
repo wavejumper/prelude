405fe884bc9ad331d584c4633a71cccb01430597
Thomas Crowley <crowley@kibu.com.au>
2015-04-28 21:56:54 +1000

    Set pretty-symbols for clojure-mode

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -9,3 +9,23 @@
 (setq tab-width 2)
 (setq-default tab-width 2)
 (setq whitespace-line-column 95)
+
+(eval-after-load 'clojure-mode
+                 '(font-lock-add-keywords
+                   'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
+                                    (0 (progn (compose-region (match-beginning 1)
+                                                              (match-end 1) "λ")
+                                              nil))))))
+(eval-after-load 'clojure-mode
+                 '(font-lock-add-keywords
+                   'clojure-mode `(("\\(#\\)("
+                                    (0 (progn (compose-region (match-beginning 1)
+                                                              (match-end 1) "ƒ")
+                                              nil))))))
+
+(eval-after-load 'clojure-mode
+                 '(font-lock-add-keywords
+                   'clojure-mode `(("\\(#\\){"
+                                    (0 (progn (compose-region (match-beginning 1)
+                                                              (match-end 1) "∈")
+                                              nil))))))
